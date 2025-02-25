3711b5cca5fca3a13842b51a72a8857abaf76afa
MapleZiyer <904532745@qq.com>
2025-02-25 16:11:38 +0800

    grpo

--- install_java.sh
+++ install_java.sh
@@ -14,13 +14,23 @@ rm jdk17.tar.gz
 # 获取解压后的目录名
 JDK_DIR=$(ls -d */ | grep -i corretto)
 
-# 添加环境变量配置到.bashrc
-echo "\n# Java Environment Variables" >> $HOME/.bashrc
-echo "export JAVA_HOME=\"$HOME/java/$JDK_DIR\"" >> $HOME/.bashrc
-echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> $HOME/.bashrc
+# 添加环境变量配置到.bashrc和.zshrc
+for rcfile in "$HOME/.bashrc" "$HOME/.zshrc"; do
+    if [ -f "$rcfile" ]; then
+        # 移除旧的Java环境变量配置
+        sed -i '/# Java Environment Variables/d' "$rcfile"
+        sed -i '/JAVA_HOME/d' "$rcfile"
 
-# 使环境变量生效
-source $HOME/.bashrc
+        # 添加新的环境变量配置
+        echo "\n# Java Environment Variables" >> "$rcfile"
+        echo "export JAVA_HOME=\"$HOME/java/$JDK_DIR\"" >> "$rcfile"
+        echo 'export PATH="$JAVA_HOME/bin:$PATH"' >> "$rcfile"
+    fi
+done
+
+# 立即设置环境变量
+export JAVA_HOME="$HOME/java/$JDK_DIR"
+export PATH="$JAVA_HOME/bin:$PATH"
 
 # 验证安装
 java -version
